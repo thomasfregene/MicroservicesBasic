b423c624a1e443704ef92c12c42cf546d8a87069
thomasfregene <fregzythomas@gmail.com>
2022-12-09 20:30:30 +0100

    platform controller actions

--- Profiles/PlatformsProfile.cs
+++ Profiles/PlatformsProfile.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PlatformService.Profiles
 {
-    public class PlatformsProfile
+    public class PlatformsProfile : Profile
     {
+        public PlatformsProfile()
+        {
+            //Source -> Target
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatformCreateDto, Platform>();
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,7 @@ namespace PlatformService
             services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
             services.AddScoped<IPlatformRepo, PlatformRepo>();
             services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
