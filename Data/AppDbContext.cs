45c9abcbdf89f4135bd3859cf6c6626d861e95e7
thomasfregene <fregzythomas@gmail.com>
2022-12-09 04:53:19 +0100

    initial commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+
+    }
+}
